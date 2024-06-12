ec2edbf2d55cea88980ddfd20f5d483aa3d1199f
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-06-12 09:38:11 +0300

    Connected Technology page to the template folder containing the nav

--- Technology.php
+++ Technology.php
@@ -8,30 +8,7 @@
     <link rel="icon" type="image/x-icon" href="Images/Background 1.jpg">
 </head>
 <body style="background-image: url(Images/Background\ 1.jpg); background-size: cover;">
-    <header>
-        <h1>Virtual Voyage</h1>
-        <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="AboutUs.html">About Us</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="destinations.html">Destinations</a></li>
-                <li><a href="FAQ.html">FAQ</a></li>
-                <li><a href="PricingAndPackages.html">Pricing and Packages</a></li>
-                <li><a href="Technology.html">Technology</a></li>
-                <li><a href="OrderDetails.html">Order Details</a></li>
-                <div class="topnav-right">
-                    <li class="dropdown">
-                        <a href="javascript:void(0)" class="dropbtn">My Account</a>
-                        <div class="dropdown-content">
-                            <a href="Sign Up.html">Sign Up</a>
-                            <a href="SignIn.html">Sign In</a>
-                        </div>
-                    </li>
-                    </div>
-            </ul>
-        </nav>
-    </header>
+<?php include_once("Template/nav.php");?>
 
     <section id="technology">
         <h2>Unveiling the Magic: How We Create Virtual Voyages</h2>
