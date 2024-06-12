7d4ea753c2d397a0d7a5a31f2abf810217a6c3c5
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-06-12 09:44:13 +0300

    Changed the file names under the nav.php to php files

--- Template/nav.php
+++ Template/nav.php
@@ -2,20 +2,20 @@
         <h1>Virtual Voyage</h1>
         <nav>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="AboutUs.html">About Us</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="destinations.html">Destinations</a></li>
-                <li><a href="FAQ.html">FAQ</a></li>
-                <li><a href="Pricings and packages.html">Pricings and Packages</a></li>
-                <li><a href="Technology.html">Technology</a></li>
-                <li><a href="OrderDetails.html">Order Details</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="AboutUs.php">About Us</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="destinations.php">Destinations</a></li>
+                <li><a href="FAQ.php">FAQ</a></li>
+                <li><a href="Pricings and packages.php">Pricings and Packages</a></li>
+                <li><a href="Technology.php">Technology</a></li>
+                <li><a href="OrderDetails.php">Order Details</a></li>
                 <div class="topnav-right">
                 <li class="dropdown">
                     <a href="javascript:void(0)" class="dropbtn">My Account</a>
                     <div class="dropdown-content">
-                        <a href="Sign Up.html">Sign Up</a>
-                        <a href="SignIn.html">Sign In</a>
+                        <a href="Sign Up.php">Sign Up</a>
+                        <a href="SignIn.php">Sign In</a>
                     </div>
                 </li>
             </div>
