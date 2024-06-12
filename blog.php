7ebdfd8a891982e339e572118cb2598f92825419
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-06-12 09:22:02 +0300

    Changed blog and destination pages to php

--- blog.php
+++ blog.php
@@ -10,31 +10,7 @@
     <title>Blog</title>
 </head>
 <body style="background-image: url(Images/Background\ 1.jpg); background-size: cover;">
-    <header>
-        <h1>Virtual Voyage</h1>
-        <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="AboutUs.html">About Us</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="destinations.html">Destinations</a></li>
-                <li><a href="FAQ.html">FAQ</a></li>
-                <li><a href="Pricings and packages.html">Pricing and Packages</a></li>
-                <li><a href="Technology.html">Technology</a></li>
-                <li><a href="OrderDetails.html">Order Details</a></li>
-                <div class="topnav-right">
-                    <li class="dropdown">
-                        <a href="javascript:void(0)" class="dropbtn">My Account</a>
-                        <div class="dropdown-content">
-                            <a href="Sign Up.html">Sign Up</a>
-                            <a href="SignIn.html">Sign In</a>
-                        </div>
-                    </li>
-                    </div>
-                
-            </ul>
-        </nav>
-    </header>
+    <?php include_once("Template/nav.php");?>
 
     <!-- Blog Section -->
     <section id="blog">
