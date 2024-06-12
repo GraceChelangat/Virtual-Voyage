b1fcc67b5e018e8a313e48072155bfa56bca4ac5
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-06-12 09:24:16 +0300

    Connected the pages with the new created folder called template connecting the nav bar

--- FAQ.php
+++ FAQ.php
@@ -9,30 +9,6 @@
 </head>
 <body style="background-image: url(Images/Background\ 1.jpg); background-size: cover;">
     <?php include_once("Template/nav.php");?>
-    <header>
-        <h1>Virtual Voyage</h1>
-        <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="AboutUs.html">About Us</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="destinations.html">Destinations</a></li>
-                <li><a href="FAQ.html">FAQ</a></li>
-                <li><a href="Pricings and packages.html">Pricing and Packages</a></li>
-                <li><a href="Technology.html">Technology</a></li>
-                <li><a href="OrderDetails.html">Order Details</a></li>
-                <div class="topnav-right">
-                    <li class="dropdown">
-                        <a href="javascript:void(0)" class="dropbtn">My Account</a>
-                        <div class="dropdown-content">
-                            <a href="Sign Up.html">Sign Up</a>
-                            <a href="SignIn.html">Sign In</a>
-                        </div>
-                    </li>
-                    </div>
-            </ul>
-        </nav>
-    </header>
 
     <!-- FAQ Section -->
     <section id="faq">
