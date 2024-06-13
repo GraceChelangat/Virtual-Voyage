0afe50d726a49da8d9cf09f549e1d273cb4dff31
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-06-13 13:25:15 +0300

    Added a header.php file

---
+++ Template/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/styles.css">
+    <link rel="icon" type="image/x-icon" href="Images/Background 1.jpg">
+    <title>Virtual Voyage</title>
+</head>
