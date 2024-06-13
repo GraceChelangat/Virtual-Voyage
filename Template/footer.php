62e873d32bdd4f389debe439ae1717c3602308b9
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-06-13 07:02:03 +0300

    Added the closing tags of body and html to the footer file in the templates folder

--- Template/footer.php
+++ Template/footer.php
@@ -1,5 +1,7 @@
-<footer>
+        <footer>
             <p>&copy; 2024 Virtual Voyage. All rights reserved.</p>
             <p>Email us at virtualvoyage@gmail.com</p>
 
         </footer>
+    </body>
+</html>
