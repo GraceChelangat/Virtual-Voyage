755b8d4b4d5288acb15331239d82181015dc5045
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-06-13 13:28:17 +0300

    Deletedthe head part of the file and a dded a php link to the header file in the template folder

--- blog.php
+++ blog.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="icon" type="image/x-icon" href="Images/Background 1.jpg">
-    <title>Blog</title>
-</head>
+<?php include_once("Template/nav.php");?>
 <body style="background-image: url(Images/Background\ 1.jpg); background-size: cover;">
     <?php include_once("Template/nav.php");?>
 
