894a4b926a3cfa619aa22650688a3eac35124325
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-06-19 19:21:30 +0300

    Esited the dropdown

--- Template/nav.php
+++ Template/nav.php
@@ -1,24 +1,22 @@
 <header>
-        <h1>Virtual Voyage</h1>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="AboutUs.php">About Us</a></li>
-                <li><a href="blog.php">Blog</a></li>
-                <li><a href="destinations.php">Destinations</a></li>
-                <li><a href="FAQ.php">FAQ</a></li>
-                <li><a href="Pricings and packages.php">Pricings and Packages</a></li>
-                <li><a href="Technology.php">Technology</a></li>
-                <li><a href="bookTours.php">Book Tour</a></li>
-                <div class="topnav-right">
-                <li class="dropdown">
-                    <a href="javascript:void(0)" class="dropbtn">My Account</a>
-                    <div class="dropdown-content">
-                        <a href="Sign Up.php">Sign Up</a>
-                        <a href="SignIn.php">Sign In</a>
-                    </div>
-                </li>
-            </div>
-            </ul>
-        </nav>
-    </header>
+  <h1>Virtual Voyage</h1>
+  <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="AboutUs.php">About Us</a></li>
+      <li><a href="blog.php">Blog</a></li>
+      <li><a href="destinations.php">Destinations</a></li>
+      <li><a href="FAQ.php">FAQ</a></li>
+      <li><a href="Pricings and packages.php">Pricings and Packages</a></li>
+      <li><a href="Technology.php">Technology</a></li>
+      <li><a href="bookTours.php">Book Tour</a></li>
+      <li><a href="termsandconditions.php">Terms and Conditions</a></li>
+      <li class="dropdown" style="float: right;"> <a href="#">My Account</a>
+        <ul class="dropdown-content">
+          <li><a href="Sign Up.php">Sign Up</a></li>
+          <li><a href="SignIn.php">Sign In</a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</header>
