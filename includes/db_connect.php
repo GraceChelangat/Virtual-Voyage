435be3724111c9bfee766cb2e1fad4bf8af67878
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-07-04 14:59:59 +0300

    Edited the db to connect to db

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,15 +1,15 @@
 <?php
-$servername ="localhost";
-$username ="root";
-$password ="";
-$db_name = "virtualvoyage";
+// Connect to database
+$servername = "localhost";
+$username = "root"; // Replace with your actual credentials
+$password = ""; // Replace with your actual password
+$dbname = "VirtualVoyage"; // Replace with your database name
 
-//create connection
-$conn = new mysqli($servername, $username, $password, $db_name);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-
-if ($conn->connect_error){
+// Check connection
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//echo"Connected successfully";
+
 ?>
