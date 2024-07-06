01424e3389d06fc58a053b4617df77aa23fdcd24
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-07-06 10:27:29 +0300

    Edited view bookings to be able to edit bookings

---
+++ View_Bookings.php
@@ -0,0 +1,60 @@
+<?php
+ include_once("Template/header.php");
+ include_once("Template/nav.php");
+
+ // Check for success message
+ if (isset($_GET['message'])) {
+     echo '<p>' . htmlspecialchars($_GET['message']) . '</p>';
+ }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "VirtualVoyage";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch all bookings
+$sql = "SELECT id, name, email, destination, details, additional_details, date FROM Book_Tours";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1'>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Destination</th>
+                <th>Details</th>
+                <th>Additional Details</th>
+                <th>Date</th>
+                <th>Actions</th>
+            </tr>";
+    // Output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>{$row['id']}</td>
+                <td>{$row['name']}</td>
+                <td>{$row['email']}</td>
+                <td>{$row['destination']}</td>
+                <td>{$row['details']}</td>
+                <td>{$row['additional_details']}</td>
+                <td>{$row['date']}</td>
+                <td>
+                    <a href='update.php?id={$row['id']}'>Update</a> |
+                    <a href='delete.php?id={$row['id']}' onclick='return confirm(\"Are you sure you want to delete this booking?\")'>Delete</a>
+                </td>
+            </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
