52f8a6654a4dc6de519864644648b815cee9aa50
Grace Chelangat <grace.chelangat@strathmore.edu>
2024-07-08 09:50:46 +0300

    m

--- FAQ.php
+++ FAQ.php
@@ -1,6 +1,7 @@
 <?php include_once("Template/header.php");?>
 <body style="background-image: url(Images/Background\ 1.jpg); background-size: cover;">
     <?php include_once("Template/nav.php");?>
+    
 
     <!-- FAQ Section -->
     <section id="faq">
